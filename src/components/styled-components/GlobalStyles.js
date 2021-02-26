a0eade67e795b8316d3a11960f8329f487f51f38
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2021-02-26 22:18:50 +0100

    Set OpenSans as default font

--- src/components/styled-components/GlobalStyles.js
+++ src/components/styled-components/GlobalStyles.js
@@ -1,9 +1,25 @@
 import { createGlobalStyle } from 'styled-components';
 
+import OpenSansRegular from '../../assets/fonts/open-sans/open-sans-regular.ttf';
+import OpenSansSemiBold from '../../assets/fonts/open-sans/open-sans-semi-bold.ttf';
+
 const GlobalStyles = createGlobalStyle`
+  @font-face {
+    font-family: 'Open Sans';
+    src: url(${OpenSansRegular}) format("truetype");
+    font-weight: 400;
+  }
+
+  @font-face {
+    font-family: 'Open Sans';
+    src: url(${OpenSansSemiBold}) format("truetype");
+    font-weight: 600;
+  }
+
   body {
     margin: 0;
     font-size: 18px;
+    font-family: 'Open Sans';
   }
 `;
 
